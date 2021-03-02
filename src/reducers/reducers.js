edcf8c8e7d3e2324ce5609e9e2d3884d069caf8d
Rmiverson <Rmiverson99@gmail.com>
2021-03-02 00:02:13 -0800

    added action and reducer for follows posts

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,5 +1,6 @@
 export const initialState = {
-   currentUser: {}
+   currentUser: {},
+   followsPosts: {}
 }
 
 export default function reducer(state = initialState, action) {
@@ -8,6 +9,8 @@ export default function reducer(state = initialState, action) {
          return {...state, currentUser: action.payload}
       case 'LOGOUT_USER':
          return {...state, currentUser: {}}
+      case 'FOLLOWS_POSTS':
+         return {...state, followsPosts: action.payload}
       default:
          return state
    }
