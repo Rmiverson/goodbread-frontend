9b39ba34489fd565f2fe9a16f3a7ba7c4e1b6037
Rmiverson <Rmiverson99@gmail.com>
2021-07-28 17:41:01 -0700

    reworked login page to be a functional component

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { userLoginFetch } from '../actions/actions'
 import { Redirect } from 'react-router-dom'
@@ -8,44 +8,44 @@ import Landing from '../components/Landing'
 
 import {ReactComponent as ReactLogo} from '../logo.svg'
 
-class Login extends React.Component {
-   state = {
-      username: "",
-      password: ""
+const Login = (props) => {
+   const [username, setUsername] = useState("")
+   const [password, setPassword] = useState("")
+
+   const handleChange = (e) => {
+      if (e.target.name === 'password') {
+         setPassword(e.target.value)
+      } else if (e.target.name === 'username') {
+         setUsername(e.target.value)
+      }
    }
 
-   handleChange = (e) => {
-      this.setState({
-         [e.target.name]: e.target.value
+   const handleSubmit = (e) => {
+      e.preventDefault()
+      props.userLoginFetch({
+         username: username,
+         password: password
       })
    }
 
-   handleSubmit = (e) => {
-      e.preventDefault()
-      this.props.userLoginFetch(this.state)
-   }
-
-
-   render() {
-      return(
-         <div className="login-form">
-            {!!this.props.currentUser.id && <Redirect to="/" />}
-            <div className="landing-logo">
-               <ReactLogo />
-            </div>
-            <Landing />
-            <UserForm type="Login" username={this.state.username} password={this.state.password} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
+   return (
+      <div className="login-form">
+         {!!props.currentUser.id && <Redirect to="/" />}
+         <div className="landing-logo">
+            <ReactLogo />
          </div>
-      )   
-   }
+         <Landing />
+         <UserForm type="Login" username={username} password={password} handleChange={handleChange} handleSubmit={handleSubmit}/>
+      </div>
+   )
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
    currentUser: state.currentUser
 })
 
-const mapDispatchToProps = dispatch => ({
-   userLoginFetch: userInfo => dispatch(userLoginFetch(userInfo))
+const mapDispatchToProps = (dispatch) => ({
+   userLoginFetch: (userInfo) => dispatch(userLoginFetch(userInfo))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login)
